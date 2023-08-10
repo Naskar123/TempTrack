a9003d640eba908d7aa51ddc4256a2a9c8927a0d
Naskar123 <82303282+Naskar123@users.noreply.github.com>
2023-08-10 17:44:03 +0530

    Update api.js
    api change

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'a5d979998c3b23a9cc9eecd96b07051c';
+const API_KEY = {api key from openweather};
 const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
 export const getWeather = async (city, country) => {
